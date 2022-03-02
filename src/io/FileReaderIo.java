dfa5850b09f6704b6dac06456411b86f17616ace
lijingye123 <2623369830qq.com>
2022-03-02 20:10:21 +0800

    xxoo

---
+++ src/io/FileReaderIo.java
@@ -0,0 +1,32 @@
+/*
+package io;
+
+import java.io.File;
+
+public class FileReaderIo {
+
+    public FileReaderIo(File file) {
+
+    }
+
+    public static void main(String[] args) {
+        File file= new File("C:\\aa\\aa.txt");
+        try {
+            FileReaderIo fr=new FileReaderIo(file);
+            char[] chars = new char[1024];
+            int len;
+            while ((len=fr.read(chars))!=-1){
+                String s = new String(chars, 0, len);
+                System.out.println(s);
+            }
+        }catch (Exception e){
+
+        }
+
+
+    }
+
+
+
+}
+*/
